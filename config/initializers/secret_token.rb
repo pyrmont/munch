0b987cd64504a9212670ca23b681e8ba12314947
Michael Camilleri <mike@inqk.net>
2012-03-07 23:11:21 +0900

    Added initial files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Munch::Application.config.secret_token = 'cfaa8a8ecdeda80d1e208a74309658213492aae3840f3a3ab5d6b223433bffb2272f21663aa156a76ca2cdcd62a50eeb694eb627671357b486be0f2fe738a79d'
